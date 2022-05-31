0d008bdcf6d842062dc8ae50fc6a421532640076
Peter Bui <peter@pbwebdev.com.au>
2022-05-31 16:22:06 +1000

    adding font awesome

--- header.php
+++ header.php
@@ -18,6 +18,8 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 		<?php wp_head(); ?>
         <link rel='stylesheet' id='cardanopressio'  href='/wp-content/themes/cardanopress-website/style.css' media='all' />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
     </head>
 
 	<body <?php body_class(); ?>>
