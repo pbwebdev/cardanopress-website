87c11d84a69969cc1ea553bd16e09772e1966f42
Peter Bui <peter@pbwebdev.com.au>
2022-05-31 14:48:55 +1000

    footer widget positions

--- footer.php
+++ footer.php
@@ -15,6 +15,50 @@ $theme = wp_get_theme( CARDANOPRESS_BOOTSTRAP_THEME_BASE );
 
 		<?php get_sidebar( 'footer' ); ?>
 
+
+
+<footer class="bd-footer py-4 py-md-5 mt-5 bg-light">
+    <div class="container py-4 py-md-5 px-4 px-md-3">
+        <div class="row">
+            <div class="col-lg-3 mb-3">
+                <a class="d-inline-flex align-items-center mb-2 link-dark text-decoration-none" href="/" aria-label="Bootstrap">
+                    <span class="fs-5">CardanoPress</span>
+                </a>
+                <ul class="list-unstyled small text-muted">
+                    <li class="mb-2">We built this plugin to help with the adoption of the Cardano blockchain.</li>
+                    <li class="mb-2">Built with an open source mindset, <a href="https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html" title="GPL2 license">GPL2</a> , just like WordPress & Cardano.</li>
+                    <li class="mb-2">Building for Cardano</li>
+                </ul>
+            </div>
+            <div class="col-6 col-lg-2 offset-lg-1 mb-3">
+
+                <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar("Footer 1") ) : ?>
+                <?php endif;?>
+
+            </div>
+            <div class="col-6 col-lg-2 mb-3">
+
+                <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar("Footer 2") ) : ?>
+                <?php endif;?>
+
+            </div>
+            <div class="col-6 col-lg-2 mb-3">
+
+                <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar("Footer 3") ) : ?>
+                <?php endif;?>
+
+            </div>
+            <div class="col-6 col-lg-2 mb-3">
+
+                <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar("Footer 4") ) : ?>
+                <?php endif;?>
+
+            </div>
+        </div>
+    </div>
+</footer>
+
+
 		<footer class="site-footer py-2 bg-dark text-light">
 			<div class="container">
 				<nav class="d-flex align-items-center justify-content-between">
@@ -22,7 +66,7 @@ $theme = wp_get_theme( CARDANOPRESS_BOOTSTRAP_THEME_BASE );
 
 					<div class="copyright">
 						<small>Copyright &copy; <?php echo esc_html( gmdate( 'Y' ) ); ?> <a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="text-light"><?php bloginfo( 'name' ); ?></a>.</small>
-						<span class="small">Designed and developed by <a href="<?php echo esc_url( $theme->get( 'AuthorURI' ) ); ?>" class="text-light"><?php echo esc_html( $theme->get( 'Author' ) ); ?></a>.</span>
+						<span class="small">Powered by <a href="<?php echo esc_url( $theme->get( 'AuthorURI' ) ); ?>" class="text-light"><?php echo esc_html( $theme->get( 'Author' ) ); ?></a>.</span>
 					</div>
 				</nav>
 			</div>
---
+++ functions.php
@@ -0,0 +1,40 @@
+<?php
+if ( function_exists('register_sidebar') )
+    register_sidebar(array(
+            'name' => 'Footer 1',
+            'before_widget' => '<div class = "footer1">',
+            'after_widget' => '</div>',
+            'before_title' => '<h3>',
+            'after_title' => '</h3>',
+        )
+    );
+
+if ( function_exists('register_sidebar') )
+    register_sidebar(array(
+            'name' => 'Footer 2',
+            'before_widget' => '<div class = "footer2">',
+            'after_widget' => '</div>',
+            'before_title' => '<h3>',
+            'after_title' => '</h3>',
+        )
+    );
+
+if ( function_exists('register_sidebar') )
+    register_sidebar(array(
+            'name' => 'Footer 3',
+            'before_widget' => '<div class = "footer3">',
+            'after_widget' => '</div>',
+            'before_title' => '<h3>',
+            'after_title' => '</h3>',
+        )
+    );
+
+if ( function_exists('register_sidebar') )
+    register_sidebar(array(
+            'name' => 'Footer 4',
+            'before_widget' => '<div class = "footer4">',
+            'after_widget' => '</div>',
+            'before_title' => '<h3>',
+            'after_title' => '</h3>',
+        )
+    );
