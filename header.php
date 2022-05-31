d20fc8c0c661bd6cf1a9ee01aa0c075224f2b819
Peter Bui <peter@pbwebdev.com.au>
2022-05-31 12:38:44 +1000

    commit theme style attachment

--- header.php
+++ header.php
@@ -17,7 +17,8 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 		<?php wp_head(); ?>
-	</head>
+        <link rel='stylesheet' id='cardanopressio'  href='/wp-content/themes/cardanopress-website/style.css' media='all' />
+    </head>
 
 	<body <?php body_class(); ?>>
 		<?php wp_body_open(); ?>
