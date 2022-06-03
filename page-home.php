4b018059ea0526d50a2454aa604c86818139f85f
Peter Bui <peter@pbwebdev.com.au>
2022-06-03 19:23:29 +1000

    h1 white and wider col

--- page-home.php
+++ page-home.php
@@ -17,7 +17,7 @@ get_header();
         <div class="container-xxl">
             <div class="col-md-8 mx-auto text-center">
                 <div class="banner">
-                    <div class="col-md-6 mx-auto">
+                    <div class="col-md-8 mx-auto">
                         <h1>Integrating Cardano & WordPress</h1>
                         <p>Learn how you can connect your WordPress website to the Cardano blockchain, creating unique integrated decentralised application experiences.</p>
                         <a class="btn btn-primary" href="/products">Learn more</a>
